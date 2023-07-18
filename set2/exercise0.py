fcdafde3bd39fa94177857bc039c1e4ac556a796
Rohaan Adnan <z5340777@ad.unsw.edu.au>
2023-07-18 18:47:36 +1000

    Weeek 2

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+
 """Modify each function so that it does what the doc string asks you to do.
 
 The command to run the tests is:
@@ -124,30 +125,39 @@ def minitest(f, args, expected):
 if __name__ == "__main__":
     """This code runs when you run this file."""
 
-    print(
-        """
-          This section does a quick test on your results and prints them nicely
-          It's NOT the official tests, they are in tests.py as usual.
-          Add to these tests if you want, give them arguments etc. to make sure that your
-          code is robust to the situations that you'll see in action.
-
+    try:
+        from helper import minitest
+        print(
+            """
+            This section does a quick test on your results and prints them nicely
+            It's NOT the official tests, they are in tests.py as usual.
+            Add to these tests if you want, give them arguments etc. to make sure that your
+            code is robust to the situations that you'll see in action.
+          the format is: minitest(function_name, [list, of, arguments], expected_result)
           the format is: minitest(function_name, [list, of, arguments], expected_result)
 
-          REMEMBER: these aren't the tests that you submit, these are just
-          there to keep you sane."""
-    )
+            the format is: minitest(function_name, [list, of, arguments], expected_result)
 
-    minitest(add_1, [1], 2)
-    minitest(add_5, [1], 6)
-    minitest(add_5, [6], 11)
-    minitest(add_5, [-3], 2)
-    minitest(add_5, [0.5], 5.5)
-    minitest(adder, [-0.5, -0.5], -1)
-    minitest(adder, [2, 2], 4)
-    minitest(adder, [2, -2], 0)
-    minitest(shout, ["hello"], "HELLO")
-    minitest(really_shout, ["hello"], "HELLO!")
-    minitest(really_shout, [""], "!")
-    minitest(really_shout, ["!"], "!!")
-    minitest(shout_with_a_number, ("hello", 42), "HELLO 42")
-    print("p.s. see note above these results")
+            REMEMBER: these aren't the tests that you submit, these are just
+            there to keep you sane."""
+        )
+    
+        minitest(add_1, [1], 2)
+        minitest(add_5, [1], 6)
+        minitest(add_5, [6], 11)
+        minitest(add_5, [-3], 2)
+        minitest(add_5, [0.5], 5.5)
+        minitest(adder, [-0.5, -0.5], -1)
+        minitest(adder, [2, 2], 4)
+        minitest(adder, [2, -2], 0)
+        minitest(shout, ["hello"], "HELLO")
+        minitest(really_shout, ["hello"], "HELLO!")
+        minitest(really_shout, [""], "!")
+        minitest(really_shout, ["!"], "!!")
+        minitest(shout_with_a_number, ("hello", 42), "HELLO 42")
+        print("p.s. see note above these results")
+    except ModuleNotFoundError as e:
+        print("⚠"*20, "\nWe're looking for a module that's missing. That's probably a problem that a tutor needs to figure out.\n")
+        print(e)
+        print("⚠"*20)
+    
--- set2/exercise3.py
+++ set2/exercise3.py
@@ -1,5 +1,10 @@
 # -*- coding: UTF-8 -*-
-"""Modify each function until the tests pass."""
+
+"""
+Welcome to exercise 3 of set 2.
+
+Modify each function until the tests pass.
+"""
 
 
 def is_odd(a_number):
@@ -74,6 +79,27 @@ def loops_1c(number_of_items=5, symbol="#"):
     or the symbol, let it be whatever it wants to be.
     """
     return [symbol] * number_of_items
+
+
+def loops_2_preview():
+    """Make a big square 💩field.
+
+    return a list of 4 items, each one a list of 4 items,
+    each one of those, a string with exacly one star in it.
+    E.g.: [
+            ['💩', '💩', '💩', '💩'],
+            ['💩', '💩', '💩', '💩'],
+            ['💩', '💩', '💩', '💩'],
+            ['💩', '💩', '💩', '💩'],
+          ]
+    """
+    field = []
+    for i in range(4):
+        row = []
+        for j in range(4):
+            row.append("💩")
+        field.append(row)
+    return field
 
 
 def loops_2():
@@ -153,16 +179,16 @@ def loops_5():
 
     Return this:
     [
-      ['(i0, j0)', '(i0, j1)', '(i0, j2)', '(i0, j3)', '(i0, j4)'],
-      ['(i1, j0)', '(i1, j1)', '(i1, j2)', '(i1, j3)', '(i1, j4)'],
-      ['(i2, j0)', '(i2, j1)', '(i2, j2)', '(i2, j3)', '(i2, j4)'],
-      ['(i3, j0)', '(i3, j1)', '(i3, j2)', '(i3, j3)', '(i3, j4)'],
-      ['(i4, j0)', '(i4, j1)', '(i4, j2)', '(i4, j3)', '(i4, j4)'],
-      ['(i5, j0)', '(i5, j1)', '(i5, j2)', '(i5, j3)', '(i5, j4)'],
-      ['(i6, j0)', '(i6, j1)', '(i6, j2)', '(i6, j3)', '(i6, j4)'],
-      ['(i7, j0)', '(i7, j1)', '(i7, j2)', '(i7, j3)', '(i7, j4)'],
-      ['(i8, j0)', '(i8, j1)', '(i8, j2)', '(i8, j3)', '(i8, j4)'],
-      ['(i9, j0)', '(i9, j1)', '(i9, j2)', '(i9, j3)', '(i9, j4)']
+      ["(i0, j0)", "(i0, j1)", "(i0, j2)", "(i0, j3)", "(i0, j4)"],
+      ["(i1, j0)", "(i1, j1)", "(i1, j2)", "(i1, j3)", "(i1, j4)"],
+      ["(i2, j0)", "(i2, j1)", "(i2, j2)", "(i2, j3)", "(i2, j4)"],
+      ["(i3, j0)", "(i3, j1)", "(i3, j2)", "(i3, j3)", "(i3, j4)"],
+      ["(i4, j0)", "(i4, j1)", "(i4, j2)", "(i4, j3)", "(i4, j4)"],
+      ["(i5, j0)", "(i5, j1)", "(i5, j2)", "(i5, j3)", "(i5, j4)"],
+      ["(i6, j0)", "(i6, j1)", "(i6, j2)", "(i6, j3)", "(i6, j4)"],
+      ["(i7, j0)", "(i7, j1)", "(i7, j2)", "(i7, j3)", "(i7, j4)"],
+      ["(i8, j0)", "(i8, j1)", "(i8, j2)", "(i8, j3)", "(i8, j4)"],
+      ["(i9, j0)", "(i9, j1)", "(i9, j2)", "(i9, j3)", "(i9, j4)"]
     ]
 
     TIP:
@@ -270,18 +296,25 @@ if __name__ == "__main__":
     # It's NOT the official tests, they are in tests.py as usual.
     # Add to these tests, give them arguments etc. to make sure that your
     # code is robust to the situations that you'll see in action.
-    print(is_odd(1), "is_odd odd")
-    print(is_odd(4), "is_odd even")
-    print(fix_it(True, True), "fix_it")
-    print(fix_it(True, False), "fix_it")
-    print(fix_it(False, True), "fix_it")
-    print(fix_it(False, False), "fix_it")
-    little_printer(loops_preview(), "loops_preview")
-    little_printer(loops_1a(), "loops_1a")
-    little_printer(loops_1c(4, "×°×"), "loops_1c")
-    little_printer(loops_2(), "loops_2")
-    little_printer(loops_3(), "loops_3")
-    little_printer(loops_4(), "loops_4")
-    little_printer(loops_5(), "loops_5")
-    little_printer(loops_6(), "loops_6")
-    little_printer(loops_7(), "loops_7")
+    try:
+        from helper import little_printer, minitest
+        minitest(is_odd, [1], True)
+        minitest(is_odd, [4], False)
+        minitest(fix_it, [True, True], "No Problem")
+        minitest(fix_it, [True, False], "WD-40")
+        minitest(fix_it, [False, True], "Duct Tape")
+        minitest(fix_it, [False, False], "No Problem")
+        little_printer(loops_preview(), "loops_preview")
+        little_printer(loops_1a(), "loops_1a")
+        little_printer(loops_1c(4, "×°×"), "loops_1c")
+        little_printer(loops_2_preview(), "loops_2_preview")
+        little_printer(loops_2(), "loops_2")
+        little_printer(loops_3(), "loops_3")
+        little_printer(loops_4(), "loops_4")
+        little_printer(loops_5(), "loops_5")
+        little_printer(loops_6(), "loops_6")
+        little_printer(loops_7(), "loops_7")
+    except ModuleNotFoundError as e:
+        print("⚠"*20, "\nWe're looking for a module that's missing. That's probably a problem that a tutor needs to figure out.\n")
+        print(e)
+        print("⚠"*20)
